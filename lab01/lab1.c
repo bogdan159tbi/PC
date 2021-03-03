72d4485183b54995d9e1e83fad165d23969b9374
bogdan159tbi <bogdanmica36@gmail.com>
2021-03-03 15:52:55 +0200

    am facut primul lab

---
+++ lab01/lab1.c
@@ -0,0 +1,37 @@
+/*
+implement cat  cmd in C
+view file content
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* pentru open(), exit() */
+#include <fcntl.h> /* O_RDWR */
+#include <errno.h> /* perror() */
+
+void fatal(char * mesaj_eroare)
+{
+    perror(mesaj_eroare);
+    exit(0);
+}
+
+int main(void)
+{
+    int file;
+    int cat;
+    char buf[1024];
+
+    file = open("content" , O_RDONLY);
+    if(file < 0)
+        fatal("nu s-a putut deschide fisierul");
+
+    lseek(file, 0, SEEK_SET);
+
+    while ((cat = read(file, buf, sizeof(buf)))) {
+        if (cat < 0)
+            fatal("Eroare la citire");
+        printf("%s\n", buf);
+    }
+
+    close(file);
+    return 0;
+}
